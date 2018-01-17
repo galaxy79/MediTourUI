f698f2dde95e539eee9278dc54cf151840651725
slibin <nit.libin@gmail.com>
2018-01-18 00:15:58 +0530

    treatments offered simple template

--- assets/js/pages/showmore.js
+++ assets/js/pages/showmore.js
@@ -1,13 +1,31 @@
+/*
+ * jQuery Shorten plugin 1.1.0
+ *
+ * Copyright (c) 2014 Viral Patel
+ * http://viralpatel.net
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
+
+/*
+** updated by Jeff Richardson
+** Updated to use strict,
+** IE 7 has a "bug" It is returning undefined when trying to reference string characters in this format
+** content[i]. IE 7 allows content.charAt(i) This works fine in all modern browsers.
+** I've also added brackets where they weren't added just for readability (mostly for me).
+*/
+
 (function($) {
     $.fn.shorten = function(settings) {
         "use strict";
 
         var config = {
-            showChars: 217,
+            showChars: 100,
             minHideChars: 10,
             ellipsesText: "...",
-            moreText: "Read More...",
-            lessText: "Show Less...",
+            moreText: "Read more...",
+            lessText: "Show less",
             onLess: function() {},
             onMore: function() {},
             errMsg: null,
